a469b0a5fb1043086a32dae73dd12a99266e7813
durgaprasad91 <durga00312@gmail.com>
2024-02-07 06:57:26 +0530

    changes

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -6,7 +6,7 @@ const Head = () => {
       <section className='head'>
         <div className='container flexSB paddingTB'>
           <div className='logo'>
-            <img src='../../../../public/images/logo_1.png' alt='' />
+            <img src='../images/globalnews.jpg' alt='' />
           </div>
           <div className='ad'>
             <img src='../images/hh.png' alt='' />
